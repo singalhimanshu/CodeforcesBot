14f41619a538a261010aa8fdf817c97ea2a8a4bd
himansingh241 <himansingh24@gmail.com>
2020-06-21 11:17:24 +0530

    Created a python script to check status of a problem during contest

---
+++ checkAC.py
@@ -0,0 +1,29 @@
+import requests
+import time
+from notify import notification
+
+URL = " https://codeforces.com/api/user.status?handle=himansingh241&from=1&count=1"
+
+req = requests.get(url=URL)
+data = req.json()
+prevSubmissionId = data['result'][0]["id"]
+
+while (True):
+    time.sleep(5)
+    req = requests.get(url=URL)
+    data = req.json()
+
+    verdict = data['result'][0]['verdict']
+    problem = data['result'][0]['problem']['index']
+    newSubmissionId = data['result'][0]["id"]
+    # For debugging purposes
+    # s = f"Problem: {problem}, Verdict: {verdict} oldId: {prevSubmissionId} newId: {newSubmissionId}"
+    # print(s)
+    if (newSubmissionId != prevSubmissionId and verdict != 'testing'):
+        print('changed')
+        prevSubmissionId = newSubmissionId
+        s = f"Problem: {problem}, Verdict: {verdict}"
+        # For debugging purposes
+        # print(s)
+        notification(s, 'Codeforces')
+
