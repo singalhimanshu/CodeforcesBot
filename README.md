58adf48b525befada9870686910e4c57a0c4a9eb
himansingh241 <himansingh24@gmail.com>
2020-06-21 11:16:41 +0530

    Simplified contest.sh because of cfparser.vim plugin

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Codeforces Offline Bot
+# Codeforces Bot
 A bot that setup the contest and practice session of vim with all the required panes pre customized.
 
 
--- template.cpp
+++ template.cpp
@@ -1,24 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long 
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
 #define inf 1e18
-#define mod 1e9+7
-#define w(x) int x; cin >> x; while(x--)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
 #define sp(x, y) fixed << setprecision(y) << x;
+#define em emplace_back
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define nl '\n'
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
 
 const int maxn = 1e6;
-
+const int mod = 1e9 + 7;
 
 void solve() {
+  ll i, j, n, m;
 
+  vl a(n);
 }
 
 int main() {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  ll test = 1;
+  /* cin >> test; */
+  while (test--)
     solve();
-    return 0;
+  return 0;
 }
