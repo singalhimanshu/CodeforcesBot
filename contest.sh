1838150cd0734ed4c7610cf4d30bbb968418fcc9
himansingh241 <himansingh24@gmail.com>
2020-06-21 11:17:53 +0530

    Simplified the script

--- contest.sh
+++ contest.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 cd ~/cp/contests &&
 mkdir $1 && cd $1 &&
-mkdir {A..E} &&
-prefix=~/cp/scriptHelper
-for i in {A..E}
-do
-    cp $prefix/test.cpp $prefix/output.txt $prefix/input.txt $prefix/Session.vim ~/cp/contests/$1/$i
-done
+xargs -n 1 cp -v ../../template.cpp<<<"./A.cpp ./B.cpp ./C.cpp ./D.cpp ./E.cpp" &&
+nvim A.cpp
